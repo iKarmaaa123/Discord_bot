e6d3cd27664b603d60978acaaf08040787c9e28b
iKarmaaa123 <waael@hotmail.co.uk>
2024-09-29 19:53:28 +0100

    updates

--- ECR/ecr.tf
+++ ECR/ecr.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "my_aws_ecr_repository" {
   name                 = "test-nodejs-application"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
