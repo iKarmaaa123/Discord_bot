296e23f27e15b6d02db6e1d8c5c0f0d5a9a04d9c
iKarmaaa123 <waael@hotmail.co.uk>
2024-09-29 20:41:27 +0100

    updates

---
+++ VPC/versions.tf
@@ -0,0 +1,20 @@
+// configure the MongoDB Atlas Provider 
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+ }
+}
+
+// setting up s3 bucket for a remote backend
+terraform {
+  backend "s3" {
+    bucket = "terraform-vpc-nodejs"
+    key    = "statefile3/terraform.tfstate"
+    region = "us-east-1"
+    }
+}
